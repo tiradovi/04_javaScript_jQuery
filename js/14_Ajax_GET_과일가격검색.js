128f65ac989d1658f89a928b24be8c685fe87ca8
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-07-30 17:38:50 +0900

    Ajax_GET 과일가격 검색

---
+++ js/14_Ajax_GET_과일가격검색.js
@@ -0,0 +1,22 @@
+$(function () {
+  //검색하기 클릭
+  $("#검색기능").click(function () {
+    $.get("../json/fruits.json", function (data) {
+      console.log("data:", data);
+      console.log("data.length:", data.length);
+      /*
+      json 데이터가 []로 시작시 .length 사용가능
+                    {}로 시작시 Object.keys() 처리후 length로 계산
+      */
+      const price = $("#price").val();
+
+      for (let i = 0; i < data.length; i++) {
+        if (price == data[i].price) {
+          $("#result").html(`${data[i].name} - ${data[i].price}`);
+        } else {
+          $("#result").html(`과일 존재X`);
+        }
+      }
+    });
+  });
+});
