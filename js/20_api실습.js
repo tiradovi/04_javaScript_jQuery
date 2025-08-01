0b80e667ea15b150053c2e831a3e43fb7d848e62
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-01 10:09:59 +0900

    홈페이지실습-로그인 실습(json 제외)

--- js/20_api실습.js
+++ js/20_api실습.js
@@ -4,8 +4,12 @@ $(function () {
       data
     ) {
       $("#result").html(`
-        <img src="${data.data[0].imageSrc}">
+        
+      
+        <img src="${data.data[0].imageSrc}"alt="사진이 없습니다....">
         학명: ${data.data[0].binomialName}
+      
+
         `);
     });
   });
