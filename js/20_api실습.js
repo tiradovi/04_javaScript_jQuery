b2ec30b3cc7d2932da922342d6c5bf1e24c86587
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-04 10:12:53 +0900

    Ajax Post개념

--- js/20_api실습.js
+++ js/20_api실습.js
@@ -3,14 +3,19 @@ $(function () {
     $.get("https://extinct-api.herokuapp.com/api/v1/animal/").done(function (
       data
     ) {
+      const animal = data.data[0];
       $("#result").html(`
-        
-      
-        <img src="${data.data[0].imageSrc}"alt="사진이 없습니다....">
-        학명: ${data.data[0].binomialName}
-      
-
-        `);
+      <div class="animal-card">
+        <img src="${animal.imageSrc}" alt="사진이 없습니다.">
+        <div class="animal-info">
+          <h2>멸종 동물 이름: ${animal.commonName}</h2>
+          <p><strong>학명:</strong> ${animal.binomialName}</p>
+          <p><strong>멸종시기:</strong> ${animal.lastRecord}</p>
+          <p>${animal.shortDesc}</p>
+          <a href="${animal.wikiLink}">관련링크 보기</a>
+        </div>
+      </div>
+    `);
     });
   });
 });
