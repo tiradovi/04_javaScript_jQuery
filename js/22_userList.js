8892ed9b9c327da76e7541c22035c00a5d452d56
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-04 12:03:47 +0900

    localStorage .get .set .parse .stiringify()

--- js/22_userList.js
+++ js/22_userList.js
@@ -5,29 +5,18 @@ $(function () {
 function showUsers() {
   //localStorage 에서 모든 데이터를 담을 수 있는 리스트 변수이름 생성
 
-  const users = []; //localStorage에 저장된 정보를 목록 형태로 users내부에 저장
+  let userList = JSON.parse(localStorage.getItem("userList") || "[]");
 
-  //username, userpw가 있으면 배열에 추가
+  // 사용자 총회원수 표시 userList.length
 
-  const username = localStorage.getItem("username");
-  const userpw = localStorage.getItem("userpw");
-
-  if (username && userpw) {
-    users.push({
-      id: username,
-      pw: userpw,
-    });
-  }
-  // 사용자 총회원수 표시 users.length
-
-  // 사용자가 없으면 users.length ===0 no-users
+  // 사용자가 없으면 userList.length ===0 no-users
 
   // map 사용해서 HTML로 소비자가 유저리스트 목록을 확인할 수 있도록 설정
-  const userHTML = users.map(
+  const userHTML = userList.map(
     (u) => `
     <div class="user-item">
-        <div class="user-id">아이디 : ${u.id}</div>
-        <div class="user-pw">비밀번호 : ${u.pw}</div>
+        <div class="user-id">아이디 : ${u.username}</div>
+        <div class="user-pw">비밀번호 : ${u.password}</div>
     </div>
     `
   );
